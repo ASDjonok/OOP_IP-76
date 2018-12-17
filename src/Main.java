001c1245a239da211a359580810af85661752d45
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2018-12-17 10:55:10 +0200

    add String splitting

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!:)");
         System.out.println("Hello World!:(");
+
+        String s = "Hello! I am robot.";
+
+        String[] split = s.split("(?<=[?!.]) ");
+//        String[] split = s.split("[?!.] ?");
+
     }
 }
